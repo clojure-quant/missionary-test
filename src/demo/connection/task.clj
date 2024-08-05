7b1a00373cbae36844006e20e7a04de0c1e40aa3
awb99 <awb99@gmail.com>
2024-08-04 19:49:31 -0500

    start order

---
+++ src/demo/connection/task.clj
@@ -0,0 +1,40 @@
+(ns demo.connection.task
+  (:require
+    [missionary.core :as m]
+    [nano-id.core :refer [nano-id]]))
+
+(def connect! 
+  (m/sp (let [id (nano-id 5)]
+          (println "connected: " id)
+          id)))
+
+(def reconnect! 
+  (m/sp (m/? (m/sleep 5000))
+        (m/? connect!)))
+
+(def poll-conn 
+  (m/ap
+   (m/amb :init)
+   (loop [v (m/? connect!)]
+     (m/amb v (recur (m/? reconnect!))))))
+
+
+(def conn
+  (m/signal poll-conn))
+
+(m/? (m/join vector
+             ;(m/reduce conj [] (m/eduction (take 1) conn))
+             (m/reduce conj [] (m/eduction (take 2) conn))
+             (m/reduce conj [] (m/eduction (take 2) conn))
+             (m/reduce conj [] (m/eduction (take 2) conn))
+             (m/reduce conj [] (m/eduction (take 2) conn))
+             (m/reduce conj [] (m/eduction (take 1) conn))
+             (m/reduce conj [] (m/eduction (take 1) conn))
+             (m/reduce conj [] (m/eduction (take 1) conn))
+             (m/sp
+              (m/? (m/sleep 1))
+              (m/? (m/reduce conj [] (m/eduction (take 2) conn))))))
+
+
+
+[["e6Jou"] ["WSEaF" "Mmhfm"] ["WSEaF" "Mmhfm"]]
