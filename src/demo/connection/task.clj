30b58289fbcd6757898f376b6147d4d904f2b7dd
awb99 <awb99@gmail.com>
2024-08-05 19:36:30 -0500

    test

--- src/demo/connection/task.clj
+++ src/demo/connection/task.clj
@@ -1,11 +1,14 @@
 (ns demo.connection.task
   (:require
     [missionary.core :as m]
-    [nano-id.core :refer [nano-id]]))
+    [nano-id.core :refer [nano-id]])
+  (:import [missionary Cancelled])
+  )
 
 
 (def connect! 
   (m/sp (let [id (nano-id 5)]
+          (m/? (m/compel (m/sleep 2000)))
                      (println "connected: " id)
                      id)
                    ))
@@ -15,12 +18,27 @@
         (m/? connect!)))
 
 
+(def shutdown!
+  (m/sp 
+   (println "shutdown!")
+   (m/? (m/sleep 5000))
+   ))
+
+
 
 (def poll-conn 
   (m/ap
-   (m/amb :init)
-   (loop [v (m/? connect!)]
-     (m/amb v (recur (m/? reconnect!))))))
+   (try 
+     (m/amb "asdf")
+   (loop [v (m/? (m/compel connect!))]
+     (m/amb v (recur (m/? reconnect!))))  
+     (catch Cancelled _ 
+       ;(println "shutting down..")
+       ;(m/? shutdown!)
+       true)
+     )
+   ))
+
 
 
 (def conn
@@ -42,3 +60,54 @@
 
 
 [["e6Jou"] ["WSEaF" "Mmhfm"] ["WSEaF" "Mmhfm"]]
+
+(defn take-first-non-nil [f]
+  ; flows dont implement deref
+  (m/eduction
+   (remove nil?)
+   (take 1)
+   f))
+
+
+(defn current-v
+  "gets the first non-nil value from the flow"
+  [f]
+  (m/reduce (fn [_r v]
+              (println "current-v: " v)
+              v) nil
+            (take-first-non-nil f)))
+
+
+(defn cont
+  "converts a discrete flow to a continuous flow. 
+    returns nil in the beginning."
+  [flow]
+  (->> flow
+       (m/reductions (fn [r v]
+                       (if v v r)) nil)
+       (m/relieve {})))
+
+
+(m/? (current-v (cont poll-conn)))
+
+
+
+(def conn
+  (m/signal 
+  (cont poll-conn) 
+   )
+  )
+
+(m/? (m/join vector
+             (m/reduce conj [] (m/eduction (take 2) conn))
+             (m/reduce conj [] (m/eduction (take 1) conn))
+             (m/reduce conj [] (m/eduction (take 2) conn))
+             (m/reduce conj [] (m/eduction (take 2) conn))
+             (m/reduce conj [] (m/eduction (take 2) conn))
+             (m/reduce conj [] (m/eduction (take 1) conn))
+             (m/reduce conj [] (m/eduction (take 1) conn))
+             (m/reduce conj [] (m/eduction (take 1) conn))
+             (m/sp
+              (m/? (m/sleep 1))
+              (m/? (m/reduce conj [] (m/eduction (take 2) conn))))))
+
